801c15097e7b48122402fdc33532804aade3cd97
Nic Moy <nicolas.moy@student.csulb.edu>
2025-05-05 23:31:45 -0700

    Working bottom up and top down passes with printed sequences

--- CSCI570_Project_Minimum_V1/SampleTestCases/basic_3.py
+++ CSCI570_Project_Minimum_V1/SampleTestCases/basic_3.py
@@ -2,6 +2,7 @@ import sys
 import time
 #import psutil
 from resource import *
+import numpy as np
 
 #////////////////////// GLOBAL VARIABLES  //////////////////////////
 letter_to_idx = {'A': 0, 'C': 1, 'G': 2, 'T': 3}
@@ -17,7 +18,7 @@ gap_penalty = 30
 
 #//////////////////////  INPUT FUNCTIONS  //////////////////////////
 
-def read_input(path="input0.txt"):
+def read_input(path="input1.txt"):
     with open(path) as file:
         lines = []
         for x in file:
@@ -57,12 +58,62 @@ def write_output(new_sequence, path="output0.txt", ):
 
 
 #//////////////////////  DP FUNCTION  //////////////////////////
-def basic_solution():
-    pass
+def basic_solution(sequence1, sequence2):
+    m = len(sequence1)
+    n = len(sequence2)
+    opt = [[0]*(n+1) for _ in range(m+1)]
+
+    #Initialize column 0 and row 0
+    for x in range(1, m+1):
+        opt[x][0] = x * gap_penalty
+    for y in range(1, n+1):
+        opt[0][y] = y * gap_penalty
+
+
+    for i in range(1,m+1):
+        for j in range(1,n+1):
+            offset1 = letter_to_idx[sequence1[i-1]]
+            offset2 = letter_to_idx[sequence2[j-1]]
+
+            opt[i][j] = min(opt[i-1][j-1] + values_table[offset1][offset2],
+                            opt[i-1][j] + gap_penalty,
+                            opt[i][j-1] + gap_penalty)
+
+    i, j = m, n
+    align_X, align_Y = [], []
+    while i > 0 or j > 0:
+        # diag?
+        if i > 0 and j > 0:
+            a = letter_to_idx[sequence1[i - 1]]
+            b = letter_to_idx[sequence2[j - 1]]
+            if opt[i][j] == opt[i - 1][j - 1] + values_table[a][b]:
+                align_X.append(sequence1[i - 1])
+                align_Y.append(sequence2[j - 1])
+                i, j = i - 1, j - 1
+                continue
+        # gap in X (left)
+        if j > 0 and opt[i][j] == opt[i][j - 1] + gap_penalty:
+            align_X.append('_')
+            align_Y.append(sequence2[j - 1])
+            j -= 1
+            continue
+        # gap in Y (up)
+        if i > 0 and opt[i][j] == opt[i - 1][j] + gap_penalty:
+            align_X.append(sequence1[i - 1])
+            align_Y.append('_')
+            i -= 1
+            continue
+    aligned_X = ''.join(reversed(align_X))
+    aligned_Y = ''.join(reversed(align_Y))
+    return opt[m][n], aligned_X, aligned_Y
+
+
 
 
 #//////////////////////  MAIN FUNCTION  //////////////////////////
 sequence1, j_nums, sequence2, k_nums = read_input()
 new_seq_1 = string_generator(sequence1, j_nums)
 new_seq_2 = string_generator(sequence2, k_nums)
-write_output(new_seq_1)
+print(basic_solution(new_seq_1, new_seq_2))
+
+#write_output(new_seq_1)
